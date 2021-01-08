1ecf89d2ec0ec00191f2ca7b335d7d4bf345da75
hetp4401 <patelhet32@gmail.com>
2021-01-08 18:09:49 -0500

    cached

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
+const request = require("request");
+const express = require("express");
+const addon = express();
+
+const NodeCache = require("node-cache");
+const cache = new NodeCache();
+
 require("dotenv").config();
-var request = require("request");
-var express = require("express");
-var addon = express();
-
-var MANIFEST = require("./manifest.json");
-
-var NodeCache = require("node-cache");
-var cache = new NodeCache();
-
-var KI_PRE = process.env.KI_PRE;
-var KI_SUF = process.env.KI_SUF;
-var SE_PRE = process.env.SE_PRE;
-var TU_PRE = process.env.TU_PRE;
-var TU_SUF = process.env.TU_SUF;
-
+const KI_PRE = process.env.KI_PRE;
+const KI_SUF = process.env.KI_SUF;
+const SE_PRE = process.env.SE_PRE;
+const TU_PRE = process.env.TU_PRE;
+const TU_SUF = process.env.TU_SUF;
+const HOST = process.env.HOST;
+const SERVER = process.env.SERVER;
 const MAX_AGE = 60 * 60 * 24 * 7;
 const LINK_TTL = 60 * 30;
+const MANIFEST = require("./manifest.json");
+const EPISODE = "Episode ";
+const DUB = " Dub";
+const SUB = " Sub";
 
-var search = (query) => {
+const search = (query) => {
   return new Promise((resolve, reject) => {
     request(SE_PRE + query, (e, r, b) => {
-      var broken = b.split("category/").slice(1);
-      var search = broken.map((x) => {
-        var idx = x.indexOf("\\");
+      const broken = b.split("category/").slice(1);
+      const search = broken.map((x) => {
+        const idx = x.indexOf("\\");
         return x.substring(0, idx);
       });
 
-      var arr = [search[0]];
+      const arr = [search[0]];
       if (search.includes(search[0] + "-dub")) {
         arr.push(search[0] + "-dub");
       }
@@ -35,57 +38,57 @@ var search = (query) => {
   });
 };
 
-var ki_title = (id) => {
+const ki_title = (id) => {
   return new Promise((resolve, reject) => {
     request(KI_PRE + id + KI_SUF, (e, r, b) => {
-      var json = JSON.parse(b);
-      var name = json.meta.name;
-      var title = name.replace(/[^a-z0-9 ]/gi, " ");
+      const json = JSON.parse(b);
+      const name = json.meta.name;
+      const title = name.replace(/[^a-z0-9 ]/gi, " ");
       resolve(title);
     });
   });
 };
 
-var get_title = async (id) => {
+const get_title = async (id) => {
   return new Promise(async (resolve, reject) => {
-    var s_title = await ki_title(id);
-    var p = await search(s_title);
+    const s_title = await ki_title(id);
+    const p = await search(s_title);
     resolve(p);
   });
 };
 
-var tunnel = (title, ep) => {
+const tunnel = (title, ep) => {
   return new Promise((resolve, reject) => {
     request(TU_PRE + title + TU_SUF + ep, (e, r, b) => {
-      var json = JSON.parse(b);
-      var source = json.openload;
-      var link = source.replace("streaming", "ajax");
+      const json = JSON.parse(b);
+      const source = json.openload;
+      const link = source.replace(HOST, SERVER);
       resolve(link);
     });
   });
 };
 
-var source = (tunnel) => {
+const source = (tunnel) => {
   return new Promise(async (resolve, reject) => {
     request(tunnel, (e, r, b) => {
-      var json = JSON.parse(b);
-      var stream = json.source[0].file;
+      const json = JSON.parse(b);
+      const stream = json.source[0].file;
       console.log(stream);
       resolve(stream);
     });
   });
 };
 
-var stream = async (title, e) => {
+const stream = async (title, e) => {
   return new Promise(async (resolve, reject) => {
-    var ep = e ? e : 1;
-    var t = await tunnel(title, ep);
-    var s = await source(t);
+    const ep = e ? e : 1;
+    const t = await tunnel(title, ep);
+    const s = await source(t);
     resolve(s);
   });
 };
 
-var respond = (res, data) => {
+const respond = (res, data) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Content-Type", "application/json");
@@ -97,25 +100,26 @@ addon.get("/manifest.json", (req, res) => {
 });
 
 addon.get("/stream/:type/:media.json", async (req, res, next) => {
-  var media = req.params.media;
-  var arr = media.split(":");
-  var id = arr[1];
-  var ep = arr[2];
+  const media = req.params.media;
+  const arr = media.split(":");
+  const id = arr[1];
+  const ep = arr[2];
 
   if (!cache.get(media)) {
     if (!cache.get(id)) {
       cache.set(id, await get_title(id), MAX_AGE);
     }
-    var t = cache.get(id);
+
+    const t = cache.get(id);
 
     cache.set(
       media,
-
       await Promise.all(
         t.map(async (x) => {
           return {
             name: "A4U",
-            title: x.replace(/[^a-z0-9 ]/gi, " "),
+            title: EPISODE + ep + (x.includes("dub") ? DUB : SUB),
+
             url: await stream(x, ep),
           };
         })
@@ -124,11 +128,15 @@ addon.get("/stream/:type/:media.json", async (req, res, next) => {
     );
   }
 
-  var e = cache.get(media);
+  const e = cache.get(media);
 
   respond(res, {
     streams: e,
   });
+});
+
+addon.get("/", (req, res) => {
+  res.send("Hey there!");
 });
 
 addon.listen(7000, () => {
