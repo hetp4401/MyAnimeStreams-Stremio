9c34945eea944a6a75a84fd25858e51baa250ba3
hetp4401 <patelhet32@gmail.com>
2021-01-07 22:30:59 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,126 @@
+require("dotenv").config();
+var request = require("request");
+var express = require("express");
+var addon = express();
+
+var MANIFEST = require("./manifest.json");
+
+var KI_PRE = process.env.KI_PRE;
+var KI_SUF = process.env.KI_SUF;
+var SE_PRE = process.env.SE_PRE;
+var TU_PRE = process.env.TU_PRE;
+var TU_SUF = process.env.TU_SUF;
+
+var search = (query) => {
+  return new Promise((resolve, reject) => {
+    request(SE_PRE + query, (e, r, b) => {
+      var broken = b.split("category/").slice(1);
+      var search = broken.map((x) => {
+        var idx = x.indexOf("\\");
+        return x.substring(0, idx);
+      });
+      resolve(search[0]);
+    });
+  });
+};
+
+var k_title = (id) => {
+  return new Promise((resolve, reject) => {
+    request(KI_PRE + id + KI_SUF, (e, r, b) => {
+      var json = JSON.parse(b);
+      var name = json.meta.name;
+      var title = name.replace(/[^a-z0-9 ]/gi, "");
+      resolve(title);
+    });
+  });
+};
+
+var get_title = async (id) => {
+  return new Promise(async (resolve, reject) => {
+    var s_title = await k_title(id);
+    var title = await search(s_title);
+    resolve(title);
+  });
+};
+
+var tunnel = (title, ep) => {
+  return new Promise((resolve, reject) => {
+    request(TU_PRE + title + TU_SUF + ep, (e, r, b) => {
+      var json = JSON.parse(b);
+      var source = json.openload;
+      var link = source.replace("streaming", "ajax");
+      resolve(link);
+    });
+  });
+};
+
+var source = (tunnel) => {
+  return new Promise(async (resolve, reject) => {
+    request(tunnel, (e, r, b) => {
+      var json = JSON.parse(b);
+      var stream = json.source[0].file;
+      console.log(stream);
+      resolve(stream);
+    });
+  });
+};
+
+var stream = async (title, ep) => {
+  return new Promise(async (resolve, reject) => {
+    var t = await tunnel(title, ep);
+    var s = await source(t);
+    resolve(s);
+  });
+};
+
+var respond = (res, data) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Content-Type", "application/json");
+  res.send(data);
+};
+
+addon.get("/manifest.json", (req, res) => {
+  respond(res, MANIFEST);
+});
+
+var cache = {};
+
+addon.get("/stream/:type/:media.json", async (req, res, next) => {
+  var media = req.params.media;
+  var arr = media.split(":").slice(1);
+  var id = arr[0];
+  var ep = arr[1];
+
+  if (!(id in cache)) {
+    var t = await get_title(id);
+    cache[id] = {
+      title: t,
+      episodes: {},
+    };
+  }
+
+  var { title, episodes } = cache[id];
+
+  if (!(ep in episodes)) {
+    var e = await stream(title, ep);
+    episodes[ep] = e;
+  }
+
+  var link = await episodes[ep];
+
+  console.log(cache);
+  respond(res, {
+    streams: [
+      {
+        name: "A4U",
+        title: title + " - " + ep,
+        url: link,
+      },
+    ],
+  });
+});
+
+addon.listen(7000, () => {
+  console.log("Add-on Repository URL: http://127.0.0.1:7000/manifest.json");
+});
